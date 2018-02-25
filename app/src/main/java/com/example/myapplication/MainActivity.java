d762387b4dfe3922b0aa11de21a4cca8942056e9
xiaopihailin <3091663581@qq.com>
2018-02-25 13:38:01 +0800

    first

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //第一次提交奥
+        //第二次提交
+    }
+}
